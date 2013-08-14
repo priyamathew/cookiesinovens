2a5ab651e86c07af60c8409d21e09fa972479468
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-14 11:22:57 -0500

    add to oven now works

--- app.js
+++ app.js
@@ -2,30 +2,41 @@ function Cookie(type, time){
   this.type = type,
   this.time = time,
   this.timeInOven = 0,
-  this.status = "raw"
+  this.status = "raw",
+  this.id = new Date().getTime()
 };
 
 var prepTable = {
   tray: [],
-  makeBatch: function(item) {
+  addItem: function(item) {
     this.tray.push(item)
   },
-  addToOven: function(oven, index) {
-    var currentBatch = findItemByIndex(index);
-    oven.batches.push(currentBatch);
-    console.log(('Food in the oven'));
-  },
-  lastItem: function() {
-    return this.tray.length-1
-  },
-  findItemByIndex: function(index) {
+  removeItem: function(index) {
     return this.tray.splice(index,1)[0]
+  },
+  findItem: function(id) {
+    var result = null;
+    $(this.tray).each(function(i,item){
+      if (item.id == id){
+        result = item;
+        return false;
+      }
+    });
+    return result;
   }
 };
 
 var oven = {
   batches: [],
 
+  addItem: function(item) {
+    if (oven.batches.length < 3){
+      this.batches.push(item);
+      alert('Food in the oven');
+    } else {
+      alert('No room in the oven!');
+    }
+  },
   bakeItems: function(batch) {
     batch.timeInOven += 1;
     if (batch.timeInOven < batch.time) {
@@ -40,7 +51,7 @@ var oven = {
     for (var i=0; i < this.batches.length; i++) {
       this.bakeItems(this.batches[i]);
     }
-  }
+  },
 };
 
 var prepTableView = {
@@ -56,31 +67,34 @@ var prepTableView = {
     $(this.formSelector).on("submit", function(event){
       event.preventDefault();
       var cookie = new Cookie($(prepTableView.batchTypeField).val(),$(prepTableView.batchTimeField).val())
-      prepTable.makeBatch(cookie);
+      prepTable.addItem(cookie);
       prepTableView.clearFormFields();
-      $(prepTableView.listSelector).append(prepTableView.render(prepTable.tray[prepTable.lastItem()]));
+      $(prepTableView.listSelector).append(prepTableView.render(cookie));
     });
   },
   render: function(item) {
-    return ["<li>" + item.type + "<form class='prep_tray_item' name='prep_tray_item' data-type=" + item.type + " data-time=" + item.time + "><input type='submit' value='Add to Oven'<form></li>"]
+    return ["<li>" + item.type + "<form class='prep_tray_item' id=" + item.id + " name='prep_tray_item' data-id=" + item.id + " ><input type='submit' value='Add to Oven'<form></li>"]
   }
 };
 
 var ovenView = {
   formSelector : ".prep_tray_item",
+  // elementSelector: "rack_0",
   init: function(){
-    console.log($(this.formSelector));
     $('body').on("submit", this.formSelector, function(event){
       event.preventDefault();
-      var type = $(this).data("type")
-      var time = $(this).data("time")
-      console.log(type);
-      console.log(time);
-
+      var id = $(this).data("id");
+      var item = prepTable.findItem(id);
+      oven.addItem(item);
+      $("#" + item.id).parent().remove();
+      var rackNumber = (oven.batches.length-1).toString();
+      console.log($("#rack_" + rackNumber.toString()));
+      $("#rack_" + rackNumber).replaceWith(ovenView.render(item));
+      prepTable.removeItem(item);
     });
   },
   render: function(item) {
-    return ["<li>" + item.type + "<form data-time=" + item.time + "><input type='submit' value='Add to Oven'<form></li>"]
+    return "<td id=rack_" + (oven.batches.length-1).toString() + ">" + item.type + " [" + item.status + "]</td>"
   }
 };
 
